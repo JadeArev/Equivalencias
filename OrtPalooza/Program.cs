593cf2b4478f1dd91ceef4be1ba645722c6dc9ce
Jade <jade.arevort@gmail.com>
2022-08-22 17:00:05 -0300

    new file

---
+++ OrtPalooza/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+            string[] VecEntradas = {"GENERAL","PREFERENCIAL","VIP","PREMIUM","FAN"};
+         
+            int[] VecPrecios =  {3500,6700,10000,15000,25000};
+
+
+    }
+  }
+}
