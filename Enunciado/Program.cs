7036f49b2731fc49df8f66268942b98fc4644585
Jade <jade.arevort@gmail.com>
2022-09-15 16:31:01 -0300

    new exercise

---
+++ Enunciado/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace HelloWorld
+{
+  class Program
+  {
+    
+
+    static string IngresarPalabra(string msj){
+
+
+    Console.WriteLine(msj);
+
+    string NuevaPalabra = Console.ReadLine();
+
+    Console.WriteLine("La palabra ingresada es : " + NuevaPalabra);
+
+    return NuevaPalabra;
+
+    }
+
+
+
+    static void Main(string[] args)
+    {
+      
+      string Palabra = IngresarPalabra("Ingrese una palabra");
+
+    }
+  }
+}
+
