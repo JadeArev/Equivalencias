dd26fea44ed853b538a69ca53a1f23c1f40cbdfc
Jade <jade.arevort@gmail.com>
2022-09-22 17:24:30 -0300

    Update Enunciado

--- Enunciado/Program.cs
+++ Enunciado/Program.cs
@@ -8,26 +8,64 @@ namespace HelloWorld
   class Program
   {
     
+      static int[] ListarNumeros(string msj){
 
-    static string IngresarNumeros(string msj){
+        Console.WriteLine(msj);
+
+        int[] ListaNumeros = new int[5];
+
+        for(  int i = 0; i < ListaNumeros.Length; i++){
 
 
-    Console.WriteLine(msj);
+          ListaNumeros[i] = int.Parse(Console.ReadLine());
 
-    int[] Numeros = int.Parse(Console.ReadLine());
+        }
 
-    Console.WriteLine("La palabra ingresada es : " + NuevaPalabra);
 
-    return NuevaPalabra;
+        Console.WriteLine("Los números de la lista son: ");
 
-    }
+        foreach(var num in ListaNumeros){
+
+          Console.WriteLine(num);
+        }
+
+        return ListaNumeros;
+
+      }
+
+    
+      static void IngresarPosiciones(string msj){
+
+        Console.WriteLine(msj);
+
+
+          int posicion1 = int.Parse(Console.ReadLine());
+          int posicion2 = int.Parse(Console.ReadLine());
+
+          if (posicion1 > 20 || posicion2 > 20){
+
+            Console.WriteLine("No se puede ingresar un número mayor a 20 posiciones");
+          }
+
+          else{
+
+            Console.WriteLine("La primera posición es: " + posicion1);
+            Console.WriteLine("La segunda posición es: " + posicion2);
+
+          }
+        
+        
+      }
+
+
 
 
 
     static void Main(string[] args)
     {
       
-      int[] NumerosIngresados = IngresarNumeros("Ingrese 20 números");
+      int[] Numeros = ListarNumeros("Ingresar 5 números");
+      IngresarPosiciones("Ingrese 2 posiciones");
 
     }
   }
