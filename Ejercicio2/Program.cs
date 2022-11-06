b29d4de8d62f61e62e41fdb696d2c4cc4c19b0c1
Jade <jade.arevort@gmail.com>
2022-11-06 20:06:46 -0300

    .

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TP1
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+            IngresarNumeros();
+
+        }
+
+        public void  IngresarNumeros()
+        {
+            int [] Numeros = new int [10];
+            int Pares = 0;
+            int Impares = 0;
+            int Ceros = 0;
+            int PrimerDocena = 0;
+            int SegundaDocena = 0;
+            int TerceraDocena = 0;
+
+
+
+            for (int i = 0; i < Numeros.Length; i++)
+            {
+                Numeros [i] = int.Parse(Console.ReadLine());
+
+            }
+            foreach (int a in Numeros)
+            {
+                if (EsPar(a) && a < 37)
+                    Pares++;
+                else if (!EsPar(a) && a < 37)
+                    Impares++;
+                if (EsCero(a))
+                    Ceros++;
+                int valorDeDecena = DocenaMiembro(a);
+                if (valorDeDecena == 1)
+                    PrimerDocena++;
+                if (valorDeDecena == 2)
+                    SegundaDocena++;
+                if (valorDeDecena == 3)
+                    TerceraDocena++;
+            }
+
+            Console.WriteLine("Los números son: ");
+            foreach (var item in Numeros)
+                Console.WriteLine(item);
+            Console.WriteLine("Se encontraron " + Pares + " numeros pares.");
+            Console.WriteLine("Se encontraron " + Impares + " numeros impares.");
+            Console.WriteLine("Se encontraron " + Ceros + " numeros iguales a 0.");
+            Console.WriteLine("Se encontraron " + PrimerDocena + " igual o menores a 12.");
+            Console.WriteLine("Se encontraron " + SegundaDocena + " correspondientes a la segunda docena.");
+            Console.WriteLine("Se encontraron " + TerceraDocena + " correspondientes a la tercer docena.");
+            
+        }
+
+
+
+        static bool EsPar(int Numero)
+        {
+            return Numero % 2 == 0;
+        }
+
+        static int DocenaMiembro(int Numero)
+        {
+            if (Numero <= 12 && Numero < 37)
+            {
+                return 1;
+            }
+            if (Numero <= 24 && Numero > 12 && Numero < 37)
+            {
+                return 2;
+            }
+            if (Numero <= 36 && Numero > 24 && Numero < 37)
+            {
+                return 3;
+            }
+            return 0;
+        }
+
+        static bool EsCero(int Numero)
+        {
+            return Numero == 0;
+        }
+
+    }
+}
--- Enunciado/Program.cs
+++ Enunciado/Program.cs
@@ -16,9 +16,7 @@ namespace HelloWorld
 
         for(  int i = 0; i < ListaNumeros.Length; i++){
 
-
           ListaNumeros[i] = int.Parse(Console.ReadLine());
-
         }
 
         Console.WriteLine("Los números de la lista son: ");
@@ -31,58 +29,42 @@ namespace HelloWorld
         return ListaNumeros;
 
       }
-
-
-
       static void IngresarPosiciones(int[] Numeros, int posicion1, int posicion2){
-
 
         if(posicion1 >= 5 || posicion2 >= 5){
 
           Console.WriteLine("Se alcanzó la máxima cantidad de números");
-
         }
 
       }
-
-
       static void MostrarPosiciones(int[] Numeros, int posicion1, int posicion2){
 
         Console.WriteLine(Numeros[posicion1]);
         Console.WriteLine(Numeros[posicion2]);
 
       }
-
-
-      static int Promedio(int[] Numeros){
-        
+      static double Promedio(int[] Numeros){
+        double Suma = 0;
+        double Divisor = 0;
         for(int i = 0; i < Numeros.Length; i++){
-
-          if(Numeros[i] % 2 == 0){
-            
+          if(i % 2 == 0){
+              Suma += Numeros[i];
+              Divisor++;
           }
-          
         }
-      
+        return Suma = Suma / Divisor;
 
+      }
     static void Main(string[] args)
     {
-
-
       int[] Numeros = ListarNumeros("Ingresar no más de 20 números");
-
-
       Console.WriteLine("siguiente, ingrese dos posiciones");
-
       int posicion1 = int.Parse(Console.ReadLine());
       int posicion2 = int.Parse(Console.ReadLine());
-
-      
-
-     IngresarPosiciones(Numeros, posicion1, posicion2); 
-
-
+      IngresarPosiciones(Numeros, posicion1, posicion2); 
      MostrarPosiciones(Numeros, posicion1, posicion2);
+     double Resultado = Promedio(Numeros);
+     Console.WriteLine("El promedio es : " + Resultado);
 
     }
   }
