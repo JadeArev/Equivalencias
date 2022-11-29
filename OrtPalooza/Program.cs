06a2c719ed958df3547626a7e4860a2f15b15a40
Jade <jade.arevort@gmail.com>
2022-11-29 16:35:32 -0300

    .

--- OrtPalooza/Program.cs
+++ OrtPalooza/Program.cs
@@ -10,8 +10,6 @@ namespace HelloWorld
             string[] VecEntradas = {"GENERAL","PREFERENCIAL","VIP","PREMIUM","FAN"};
          
             int[] VecPrecios =  {3500,6700,10000,15000,25000};
-
-
     }
   }
 }
