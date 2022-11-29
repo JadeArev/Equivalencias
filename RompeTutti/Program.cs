0df0e8a52d1ee55b20ef8fc83127064dbd7673e2
Jade <jade.arevort@gmail.com>
2022-11-29 17:15:00 -0300

    .

--- RompeTutti/Program.cs
+++ RompeTutti/Program.cs
@@ -6,10 +6,30 @@ namespace TP1
 {
     public static class Program
     {
-        
-        static void Main(string[] args)
-        {
 
+    static string[] IngresarModelos(string msj){
+
+    Console.WriteLine(msj);
+   
+    string[] Modelos = new string[10];
+        
+        for (string i = 0; i < Modelos.Length; i++)
+        {
+            Modelos[i] = Console.ReadLine();
+        }  
+
+    foreach(string modelo in Modelos){
+
+        Console.WriteLine(modelo);
+    }
+        return Modelos;
+    }
+
+
+        static void Main(string[] args)
+
+        {
+            string[] ModelosIngreados = IngresarModelos("Ingresar 10 modelos de autos");
         }   
         }
     }
