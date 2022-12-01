330f4342d931d05709125952efc698c6439d0c08
Jade <jade.arevort@gmail.com>
2022-12-01 13:42:34 -0300

    .

--- Tomasito/Program.cs
+++ Tomasito/Program.cs
@@ -7,26 +7,34 @@ namespace TP1
     public static class Program
     {
 
-        static void BuscarEstudiantes(int Cursos){
+        static int EstudiantesXCurso(int Cursos){
 
+            int Total = 0;
+            int SinTomasito = 0;
 
             for(int i = 0; i < Cursos; i++){
             
             Console.WriteLine("Ingrese la cantidad de alumnos para el curso");
-            int CantEstudiantes = int.Parse(Console.ReadLine());
+             int CantEstudiantes = int.Parse(Console.ReadLine());
+
+
+                Total += CantEstudiantes;
+                SinTomasito = Total - 1;
+                
             }
 
-            
+            Console.WriteLine("La cantidad de alumnos total es (sin Tomasito):" + SinTomasito);
+
+            return SinTomasito; 
         }
 
 
         static void Main(string[] args)
 
         {
-
-            TotalEstudiantes = 0;
             int Cursos = 4;
-           BuscarEstudiantes(Cursos);
+           int TotalAlumnos = EstudiantesXCurso(Cursos);
+           
         }   
         }
     }
