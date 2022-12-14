0fafa5c0da3158c74007d00206e7058c51af0a90
Jade <jade.arevort@gmail.com>
2022-12-14 17:04:28 -0300

    .

--- OrtPalooza/Program.cs
+++ OrtPalooza/Program.cs
@@ -6,23 +6,29 @@ namespace HelloWorld
   {
      static void Main(string[] args)
     {
+
+      const int DIAS_EV = 4;
       int[] Precios = {3500, 6700, 10000, 15000, 25000};
       string[] Entradas = {"GENERAL", "PREFERENCIAL", "VIP", "PREMIUM", "FAN"};
+      
+      int[] CantEntradasVendidas = new int[5] {0,0,0,0,0};
+      int[] RecaudacionXDia = new int[DIAS_EV] {0,0,0,0}; //nO puedo sumarle cosas al NULL PORQUE NO EXISTE Y POR ESO LO INICIALIZO.
       
       int dni = 0;
       int dia = 0;
       string tipoEntrada = "";
       int cantEntradas = 0;
-      int MasGasto = 0;
-      int dniMasGasto = 0;
-      int PrecioEntrada = 0;
+      int PrecioEntrada = 0; //Lo uso para poder guardar y mostrar el elemento que coincida con la misma posición del array de entradas.
 
-      IngresarVentas(dni, dia, tipoEntrada, cantEntradas, Precios, Entradas, PrecioEntrada);
+      IngresarVentas(dni, dia, tipoEntrada, cantEntradas, Precios, Entradas, PrecioEntrada, RecaudacionXDia, CantEntradasVendidas);
     }
-    static void IngresarVentas(int dni, int dia, string tipoEntrada, int cantEntradas, int[]Precios, string[] Entradas, int PrecioEntrada){
-
-      Console.WriteLine("Ingrese su DNI");
-      dni = int.Parse(Console.ReadLine());
+    static void IngresarVentas(int dni, int dia, string tipoEntrada, int cantEntradas, int[]Precios, string[] Entradas, int PrecioEntrada, int[] RecaudacionXDia, int[] CantEntradasVendidas){
+        int MasGasto = 0;   
+        int dniMasGasto = 0;
+        int posicion = 0;
+      
+        Console.WriteLine("Ingrese su DNI");
+        dni = int.Parse(Console.ReadLine());
        
         while(dni != -1){
 
@@ -38,15 +44,69 @@ namespace HelloWorld
           if(tipoEntrada == Entradas[i]){
 
             PrecioEntrada = Precios[i];
-
-          Console.WriteLine(dni + " gastó " + PrecioEntrada + " pesos por entrada " + tipoEntrada);
-
-        //Acá llamó funciones
           }
         }
+         
+         if(PrecioEntrada * cantEntradas > MasGasto){
+          MasGasto = PrecioEntrada * cantEntradas;
+          dniMasGasto = dni;
+         }
+
+        Console.WriteLine(dni + " gastó " + PrecioEntrada * cantEntradas + " pesos por " + cantEntradas + " entrada/s " + tipoEntrada + " para el día " + dia);        
+        RecaudacionXDia = RecaudacionPorDia(RecaudacionXDia, PrecioEntrada, cantEntradas, dia);
+         posicion = Posicion(tipoEntrada, Entradas);
+        CantEntradasVendidas = MasVendidas(posicion, cantEntradas, CantEntradasVendidas);
+
+        //
         Console.WriteLine("Ingrese su DNI");
         dni = int.Parse(Console.ReadLine());
         }
+
+      
+
+        Console.WriteLine("la persona que más gastó hasta ahora es " + dniMasGasto + " cuyo gasto fue de " + MasGasto);
+
+        for(int i = 0; i < RecaudacionXDia.Length; i++){
+          Console.WriteLine("La recaudación por el día " + (i+1) + " es de " + RecaudacionXDia[i]);
+        }
+        for(int i = 0; i < CantEntradasVendidas.Length; i++){
+          Console.WriteLine("La cantidad de entradas vendidas para el tipo " + Entradas[i] + " es " + CantEntradasVendidas[i]);
+        }        
+      }
+      static int[] RecaudacionPorDia(int[] RecaudacionXDia, int PrecioEntrada, int cantEntradas, int dia){
+
+        int recaudacion = PrecioEntrada * cantEntradas;
+
+       RecaudacionXDia[dia-1] += recaudacion;
+      
+      return RecaudacionXDia;
+      }
+
+      static int Posicion(string tipoEntrada, string[] Entradas){
+
+          int posicion = -1;
+          int i = 0;
+          bool aparecio = false;
+
+        while(i < Entradas.Length && !aparecio){
+
+          if(tipoEntrada == Entradas[i]){
+
+            posicion = i;
+            aparecio = true;
+          }
+          i++;
+        }
+        return posicion;
+
+      }
+      static int[] MasVendidas(int posicion, int cantEntradas, int[] CantEntradasVendidas){
+
+         CantEntradasVendidas[posicion] += cantEntradas;
+        return CantEntradasVendidas;
+      }
+
+
       }
     }
-  }
+  
